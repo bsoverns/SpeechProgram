66c5bfb616a21c3beefdb12f44d67612ff2e010e
Brad <bsoverns@gmail.com>
2022-10-23 15:39:40 -0400

    First Push

---
+++ Speech Test/Speech Test/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Speech_Test
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSpeak = new System.Windows.Forms.Button();
+            this.txtWords = new System.Windows.Forms.TextBox();
+            this.btnListen = new System.Windows.Forms.Button();
+            this.btnStopListening = new System.Windows.Forms.Button();
+            this.txtAllSpeech = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSpeak
+            // 
+            this.btnSpeak.Location = new System.Drawing.Point(289, 260);
+            this.btnSpeak.Name = "btnSpeak";
+            this.btnSpeak.Size = new System.Drawing.Size(75, 23);
+            this.btnSpeak.TabIndex = 0;
+            this.btnSpeak.Text = "Speak";
+            this.btnSpeak.UseVisualStyleBackColor = true;
+            this.btnSpeak.Click += new System.EventHandler(this.btnSpeak_Click);
+            // 
+            // txtWords
+            // 
+            this.txtWords.Location = new System.Drawing.Point(13, 29);
+            this.txtWords.Multiline = true;
+            this.txtWords.Name = "txtWords";
+            this.txtWords.Size = new System.Drawing.Size(351, 60);
+            this.txtWords.TabIndex = 1;
+            // 
+            // btnListen
+            // 
+            this.btnListen.Location = new System.Drawing.Point(12, 260);
+            this.btnListen.Name = "btnListen";
+            this.btnListen.Size = new System.Drawing.Size(75, 23);
+            this.btnListen.TabIndex = 2;
+            this.btnListen.Text = "Listen";
+            this.btnListen.UseVisualStyleBackColor = true;
+            this.btnListen.Click += new System.EventHandler(this.btnListen_Click);
+            // 
+            // btnStopListening
+            // 
+            this.btnStopListening.Enabled = false;
+            this.btnStopListening.Location = new System.Drawing.Point(93, 260);
+            this.btnStopListening.Name = "btnStopListening";
+            this.btnStopListening.Size = new System.Drawing.Size(75, 23);
+            this.btnStopListening.TabIndex = 3;
+            this.btnStopListening.Text = "Stop";
+            this.btnStopListening.UseVisualStyleBackColor = true;
+            this.btnStopListening.Click += new System.EventHandler(this.btnStopListening_Click);
+            // 
+            // txtAllSpeech
+            // 
+            this.txtAllSpeech.Location = new System.Drawing.Point(13, 120);
+            this.txtAllSpeech.Multiline = true;
+            this.txtAllSpeech.Name = "txtAllSpeech";
+            this.txtAllSpeech.Size = new System.Drawing.Size(351, 125);
+            this.txtAllSpeech.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Recognized Request";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Text Captured";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(376, 295);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAllSpeech);
+            this.Controls.Add(this.btnStopListening);
+            this.Controls.Add(this.btnListen);
+            this.Controls.Add(this.txtWords);
+            this.Controls.Add(this.btnSpeak);
+            this.Name = "frmMain";
+            this.Text = "Speech Recognition and Speaking";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSpeak;
+        private System.Windows.Forms.TextBox txtWords;
+        private System.Windows.Forms.Button btnListen;
+        private System.Windows.Forms.Button btnStopListening;
+        private System.Windows.Forms.TextBox txtAllSpeech;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Speech Test/Speech Test/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Text;
+using System.Timers;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Speech_Test
+{
+    public partial class frmMain : Form
+    {
+        SpeechSynthesizer voice = new SpeechSynthesizer();
+        SpeechRecognitionEngine inputVoice = new SpeechRecognitionEngine();
+        public frmMain()
+        {
+            InitializeComponent();            
+        }       
+        
+        
+        private void btnSpeak_Click(object sender, EventArgs e)
+        {
+            speak();
+        }
+
+        private void btnListen_Click(object sender, EventArgs e)
+        {
+            btnListen.Enabled = false;        
+            inputVoice.RecognizeAsync(RecognizeMode.Multiple);
+            btnStopListening.Enabled = true;
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            Choices commands = new Choices();
+            commands.Add(new string[] { "Yellow", "Green", "Gray", "Close program", "Do you know Zach", "Do you know Kyle", "Do you know Brad", "Do you know Brian", "Do you know Dana", "Do you know Ed", "Do you know Pito", "Do you know Rich", "Do you know Jared", "Do you know Karen", "Do you know Carla", "Do you know Ron", "Do you know Ricky", "Do you know Brondo", "What are you working on", "Do you know Tiffanie", "Do you know Sophia", "Do you know Brayden", "Do you know Leo", "Do you know Rajan", "Who is this"});
+            GrammarBuilder gBuilder = new GrammarBuilder();
+            gBuilder.Append(commands);
+            Grammar grammer = new Grammar(gBuilder);
+
+            inputVoice.LoadGrammarAsync(grammer);
+            inputVoice.SetInputToDefaultAudioDevice();
+            inputVoice.SpeechRecognized += inputVoice_SpeechRecognized;
+            
+        }
+
+        private void speak()
+        {
+            voice.SelectVoiceByHints(VoiceGender.Male);
+            voice.SpeakAsync(txtWords.Text.Replace("\r\n", " "));
+        }
+
+        private void inputVoice_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch(e.Result.Text)
+            {
+                case "Do you know Brayden":
+                    txtWords.Text = "I know Brayden, he is a lazy piece of shit";
+                    speak();
+                    break;
+                case "Yellow":
+                    txtWords.Text = "You said yellow";
+                    btnListen.BackColor = Color.Yellow;
+                    break;
+                case "Green":
+                    txtWords.Text = "You said green";
+                    btnListen.BackColor = Color.Green;
+                    break;
+                case "Gray":
+                    txtWords.Text = "You said gray";
+                    btnListen.BackColor = Color.Gray;
+                    break;
+                case "Do you know Leo":
+                    txtWords.Text = "I know Leo, he is the king of bowling";
+                    speak();
+                    break;
+                case "Do you know Rajan":
+                    txtWords.Text = "I know Rajan, he is the master of the scrums";
+                    speak();
+                    break;
+                case "Do you know Zach":
+                    txtWords.Text = "I know Zach, he used to have great hair and he likes Fit Crunch bars";
+                    speak();
+                    break;
+                case "Do you know Kyle":
+                    txtWords.Text = "I know Kyle, he wears glasses and likes chimichangas";
+                    speak();
+                    break;
+                case "Do you know Brad":
+                    txtWords.Text = "I know Brad, he eats his own poop";
+                    speak();
+                    break;
+                case "Do you know Brian":
+                    txtWords.Text = "I know Brian, he wears cool glasses";
+                    speak();
+                    break;
+                case "Do you know Dana":
+                    txtWords.Text = "I know Dana, she has an M and M waterfall fountain";
+                    speak();
+                    break;
+                case "Do you know Ed":
+                    txtWords.Text = "I know Ed, the goonies found his treasure";
+                    speak();
+                    break;
+                case "Do you know Pito":
+                    txtWords.Text = "I know P 2, he likes origamy";
+                    speak();
+                    break;
+                case "Do you know Rich":
+                    txtWords.Text = "I know Rich, he is a big fan of LeBron Jones of the Las Angeles Lakers";
+                    speak();
+                    break;
+                case "Do you know Jared":
+                    txtWords.Text = "I know Jared, he is extremely large and has poor circulation.  Every day at 4:45 he gets his pump shake started";
+                    speak();
+                    break;
+                case "Do you know Karen":
+                    txtWords.Text = "I know Karen, she likes Mike Tomlin";
+                    speak();
+                    break;
+                case "Do you know Carla":
+                    txtWords.Text = "I know Carla, she likes Medfusion";
+                    speak();
+                    break;
+                case "Do you know Ron":
+                    txtWords.Text = "I know Ron, he is the captain of a small boat";
+                    speak();
+                    break;
+                case "Do you know Brondo":
+                    txtWords.Text = "It's got electrolytes";
+                    speak();
+                    break;
+                case "What are you working on":
+                    txtWords.Text = "Get out of my area.  I am trying to catch up on my sleep";
+                    speak();
+                    break;
+                case "Do you know Tiffanie":
+                    txtWords.Text = "I know Tiffanie.  She is the most beautiful woman in the world";
+                    speak();
+                    break;
+                case "Do you know Sophia":
+                    txtWords.Text = "I know Sophia.  She doesn't like to eat her dinner";
+                    speak();
+                    break;
+                case "Who is this":
+                    txtWords.Text = "Wouldn't you like to know";
+                    speak();
+                    break;                    
+                case "Close program":
+                    this.Close();
+                    break;                    
+                default:
+                    txtWords.Text = e.Result.Text;
+                    break;
+            }
+        }
+
+        private void btnStopListening_Click(object sender, EventArgs e)
+        {
+            btnStopListening.Enabled = false;
+            inputVoice.RecognizeAsyncStop(); 
+            btnListen.Enabled = true;
+        }
+
+
+    }
+}
